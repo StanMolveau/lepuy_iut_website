e2b7e6cd7ec34b9a68ee8a2519ccfbbfa75dfdb9
Grégoire Molveau <gregoire.molveau@gmail.com>
2015-04-06 00:29:59 +0200

    scrollTo
    ajout du scrollTo en jquery

--- function.js
+++ function.js
@@ -72,3 +72,11 @@ $( document ).ready(function() {
 		$(".sousmenu-infos").removeClass("hidden");
 	});
 });
+$(document).ready(function() {
+	$('.scrollTo').click( function() { // Au clic sur un élément
+		var page = $(this).attr('href'); // Page cible
+		var speed = 750; // Durée de l'animation (en ms)
+		$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+		return false;
+	});
+});
