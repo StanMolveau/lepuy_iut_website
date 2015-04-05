c36074843fd1e67306e7ff5f9958c36e6848a9e9
Grégoire Molveau <gregoire.molveau@gmail.com>
2015-04-05 17:13:37 +0200

    add jquery functions, menus
    ajout jquery fonctionsde resize des menus, sousmenus

--- function.js
+++ function.js
@@ -3,9 +3,19 @@ $( document ).ready(function() {
 		$("#accueil").toggleClass("hidden");
 		$("#dut").toggleClass("hidden");
 		$("#licence").toggleClass("hidden");
-		$("#infos").toggleClass("hidden");	
+	}
+	function resizeMenus(){
+		$(".menu-accueil").removeClass("col-md-3");
+		$(".menu-dut").removeClass("col-md-3");
+		$(".menu-licence").removeClass("col-md-3");
+		$(".menu-infos").removeClass("col-md-3");
+		$(".menu-accueil").addClass("col-md-1");
+		$(".menu-dut").addClass("col-md-1");
+		$(".menu-licence").addClass("col-md-1");
+		$(".menu-infos").addClass("col-md-1");
 	}
 	$(".menu-accueil").click(function(){
+		resizeMenus();
 		$(".wrap1").addClass("pull-right");
 		$(".wrap2").addClass("pull-right");
 		$(".wrap3").addClass("pull-right");
@@ -13,8 +23,13 @@ $( document ).ready(function() {
 		$("#dut").addClass("hidden");
 		$("#licence").addClass("hidden");
 		$("#infos").addClass("hidden");
+		$(".sousmenu-accueil").removeClass("hidden");
+		$(".sousmenu-dut").addClass("hidden");
+		$(".sousmenu-licence").addClass("hidden");
+		$(".sousmenu-infos").addClass("hidden");
 	});
 	$(".menu-dut").click(function(){
+		resizeMenus();
 		$(".wrap1").removeClass("pull-right");
 		$(".wrap2").addClass("pull-right");
 		$(".wrap3").addClass("pull-right");
@@ -22,9 +37,14 @@ $( document ).ready(function() {
 		$("#dut").removeClass("hidden");
 		$("#licence").addClass("hidden");
 		$("#infos").addClass("hidden");
+		$(".sousmenu-accueil").addClass("hidden");
+		$(".sousmenu-dut").removeClass("hidden");
+		$(".sousmenu-licence").addClass("hidden");
+		$(".sousmenu-infos").addClass("hidden");
 	});
 
 	$(".menu-licence").click(function(){
+		resizeMenus();
 		$(".wrap1").removeClass("pull-right");
 		$(".wrap2").removeClass("pull-right");
 		$(".wrap3").addClass("pull-right");
@@ -32,8 +52,13 @@ $( document ).ready(function() {
 		$("#dut").addClass("hidden");
 		$("#licence").removeClass("hidden");
 		$("#infos").addClass("hidden");
+		$(".sousmenu-accueil").addClass("hidden");
+		$(".sousmenu-dut").addClass("hidden");
+		$(".sousmenu-licence").removeClass("hidden");
+		$(".sousmenu-infos").addClass("hidden");
 	});
 	$(".menu-infos").click(function(){
+		resizeMenus();
 		$(".wrap1").removeClass("pull-right");
 		$(".wrap2").removeClass("pull-right");
 		$(".wrap3").removeClass("pull-right");
@@ -41,5 +66,9 @@ $( document ).ready(function() {
 		$("#dut").addClass("hidden");
 		$("#licence").addClass("hidden");
 		$("#infos").removeClass("hidden");
+		$(".sousmenu-accueil").addClass("hidden");
+		$(".sousmenu-dut").addClass("hidden");
+		$(".sousmenu-licence").addClass("hidden");
+		$(".sousmenu-infos").removeClass("hidden");
 	});
 });
