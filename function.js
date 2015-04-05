27e8737c461702ae5595d988d40e2fd76eff2712
StanMolveau <neostan49@gmail.com>
2015-04-05 14:34:19 +0200

    Condition
    Add condition , non fonctionnel

--- function.js
+++ function.js
@@ -1,12 +1,70 @@
 $( document ).ready(function() {
 
+	var Adroitedut=false;
+	var Adroiteaccueil=false;
+	var Adroitelicence=false;
+	var Adroiteinfos=false;
 	$(".menu-dut").click(function(){
-		$(".menu-licence").toggleClass("pull-right");
-		$(".menu-infos").toggleClass("pull-right");
+		if(Adroitelicence=false)
+		{
+			$(".menu-licence").toggleClass("pull-right");
+			Adroitelicence=true;
+		}
+		if (Adroiteinfos=false)
+		{
+			$(".menu-infos").toggleClass("pull-right");
+			Adroiteinfos=true;
+		}
 		$("#accueil").toggleClass("hidden");
+		$("#dut").toggleClass("show");
+		$("#licence").toggleClass("hidden");
+		$("#infos").toggleClass("hidden");
+
+	});
+	$(".menu-accueil").click(function(){
+		
+		if(Adroitelicence=false)
+		{
+			$(".menu-licence").toggleClass("pull-right");
+			Adroitelicence=true;
+		}
+		
+		if (Adroiteinfos=false)
+		{
+			
+			$(".menu-infos").toggleClass("pull-right");
+			Adroiteinfos=true;
+		}
+
+		
+		if (Adroitedut=false)
+		{
+			
+			$(".menu-dut").toggleClass("pull-right");
+			Adroitedut=true;
+		}
+		$("#accueil").toggleClass("show");
 		$("#dut").toggleClass("hidden");
 		$("#licence").toggleClass("hidden");
 		$("#infos").toggleClass("hidden");
 	});
+	$(".menu-licence").click(function(){
+		if (Adroiteinfos=false)
+		{
+			
+			$(".menu-infos").toggleClass("pull-right");
+			Adroiteinfos=true;
+		}
+		$("#accueil").toggleClass("hidden");
+		$("#dut").toggleClass("hidden");
+		$("#licence").toggleClass("show");
+		$("#infos").toggleClass("hidden");
+	});
+	$(".menu-infos").click(function(){
+		$("#accueil").toggleClass("hidden");
+		$("#dut").toggleClass("hidden");
+		$("#licence").toggleClass("hidden");
+		$("#infos").toggleClass("show");
 
+	});
 });
